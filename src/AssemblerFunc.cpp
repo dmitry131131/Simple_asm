80fb75be11c7223590cff479c61870a50b59a690
dmitry131131 <antontsev.dmitry@yandex.ru>
2023-10-20 18:47:06 +0300

    Made first simple version of codegen

--- src/AssemblerFunc.cpp
+++ src/AssemblerFunc.cpp
@@ -13,12 +13,18 @@
 #include "AsmOut.h"
 #include "AssemblerFunc.h"
 
+#define DEF_CMD(name, num, args, ...)           \
+    else if (!strcmp(command, name))            \
+    {                                           \
+        __VA_ARGS__                             \
+    }                                           
+
 enum asmErrorCode main_assembler_function(textData* text)
 {
     assert(text);
 
     outputBuffer binBuffer  = {};
-    asmErrorCode error = NO_ASSEMBLER_ERRORS;
+    asmErrorCode error      = NO_ASSEMBLER_ERRORS;
 
     if ((error = buffer_ctor(&binBuffer, text->bufferSize * 4)))
     {
@@ -75,260 +81,14 @@ enum asmErrorCode main_assembler_function(textData* text)
             add_tag_to_buffer((command + 1), binBuffer.bufferPointer, &tagBuffer);
         }
 
-        else if (!strcmp(command, "jmp"))
-        {
-            fprintf(outputTextFile, "99 ");
+        #include "Functions.def"
 
-            if ((error = write_char_to_buffer(&binBuffer, 13)))
-            {
-                break;
-            }
-
-            int tagIp = -1;
-            if (sscanf(text->linesPtr[i], "%s %d", command, &tagIp) == 2)
-            {
-                fprintf(outputTextFile, "%d\n", tagIp);
-            
-                if ((error = write_int_to_buffer(&binBuffer, tagIp)))
-                {
-                    break;
-                }
-                continue;
-            }
-
-            char tagName[MAX_COMMAND_LEN] = {};
-
-            if (sscanf(text->linesPtr[i], "%s %s", command, tagName) != 2)
-            {
-                error = INVALID_SYNTAX;
-                print_assembler_error(error, text->linesPtr[i], i+1);
-                break;
-            }
-
-            tagIp = -1;
-            if ((error = get_tag_ip(&tagBuffer, tagName, &tagIp)))
-            {
-                print_assembler_error(error, text->linesPtr[i], i+1);
-                break;
-            }
-
-            fprintf(outputTextFile, "%d\n", tagIp);
-            
-            if ((error = write_int_to_buffer(&binBuffer, tagIp)))
-            {
-                break;
-            }
-
-        }
-
-        else if (!strcmp(command, "push"))
-        {
-            char cmd = 0;
-
-            if(sscanf(text->linesPtr[i], "%s %lf", command, &commandArg) == 2)
-            {
-                cmd |= 1 << 0;
-                cmd |= 1 << 4;
-
-                fprintf(outputTextFile, "17 %lf\n", commandArg);
-
-                if ((error = write_char_to_buffer(&binBuffer, cmd)))
-                {
-                    break;
-                }
-
-                if ((error = write_double_to_buffer(&binBuffer, commandArg)))
-                {
-                    break;
-                }
-            }
-
-            else if (sscanf(text->linesPtr[i], "%s %s", command, registerName) == 2)
-            {
-                cmd |= 1 << 0;
-                cmd |= 1 << 5;
-                registerNames reg = NO_NAME_REG;
-
-                fprintf(outputTextFile, "33 ");
-
-                if ((error = write_char_to_buffer(&binBuffer, cmd)))
-                {
-                    break;
-                }
-
-                if (registerName[0] == 'r' && registerName[2] == 'x')
-                {
-                    switch (registerName[1])
-                    {
-                    case 97:
-                        reg = RAX;
-                        break;
-                    case 98:
-                        reg = RBX;
-                        break;
-                    case 99:
-                        reg = RCX;
-                        break;
-                    case 100:
-                        reg = RDX;
-                        break;
-                    
-                    default:
-                        error = WRONG_REGISTER_NAME;
-                        print_assembler_error(error, text->linesPtr[i], i+1);
-                        break;
-                    }
-                }
-                else
-                {
-                    error = INVALID_SYNTAX;
-                    print_assembler_error(error, text->linesPtr[i], i+1);
-                    break;
-                }
-
-                if (error) break;
-
-                fprintf(outputTextFile, "%d\n", reg);
-
-                if ((error = write_char_to_buffer(&binBuffer, reg)))
-                {
-                    break;
-                }
-            }
-
-            else
-            {
-                error = INVALID_SYNTAX;
-                print_assembler_error(error, text->linesPtr[i], i+1);
-                break;
-            }
-        }
-
-        else if (!strcmp(command, "pop"))
-        {
-            if (sscanf(text->linesPtr[i], "%s %s", command, registerName) != 2)
-            {
-                error = INVALID_SYNTAX;
-                print_assembler_error(error, text->linesPtr[i], i+1);
-                break;
-            }
-
-            char cmd = 0;
-
-            cmd |= 1 << 0;
-            cmd |= 1 << 2;
-            cmd |= 1 << 3;
-            cmd |= 1 << 5;
-            registerNames reg = NO_NAME_REG;
-
-            fprintf(outputTextFile, "45 ");
-
-            if ((error = write_char_to_buffer(&binBuffer, cmd)))
-            {
-                break;
-            }
-
-            if (registerName[0] == 'r' && registerName[2] == 'x')
-            {
-                switch (registerName[1])
-                {
-                case 97:
-                    reg = RAX;
-                    break;
-                case 98:
-                    reg = RBX;
-                    break;
-                case 99:
-                    reg = RCX;
-                    break;
-                case 100:
-                    reg = RDX;
-                    break;
-                
-                default:
-                        error = WRONG_REGISTER_NAME;
-                        print_assembler_error(error, text->linesPtr[i], i+1);
-                        break;
-                    }
-            }
-            else
-            {
-                error = INVALID_SYNTAX;
-                print_assembler_error(error, text->linesPtr[i], i+1);
-                break;
-            }
-
-            if (error) break;
-
-            fprintf(outputTextFile, "%d\n", reg);
-
-            if ((error = write_char_to_buffer(&binBuffer, reg)))
-            {
-                break;
-            }
-        }
-
-        else if (!strcmp(command, "add"))
-        {
-            WRITE_SINGLE_COMMAND(2);
-        }
-
-        else if (!strcmp(command, "sub"))
-        {
-            WRITE_SINGLE_COMMAND(3);
-        }
-        
-        else if (!strcmp(command, "mul"))
-        {
-            WRITE_SINGLE_COMMAND(4);
-        }
-        
-        else if (!strcmp(command, "div"))
-        {
-            WRITE_SINGLE_COMMAND(5);
-        }
-        
-        else if (!strcmp(command, "out"))
-        {
-            WRITE_SINGLE_COMMAND(6);
-        }
-        
-        else if (!strcmp(command, "sqrt"))
-        {
-            WRITE_SINGLE_COMMAND(7);
-        }
-        
-        else if (!strcmp(command, "sin"))
-        {
-            WRITE_SINGLE_COMMAND(8);
-        }
-        
-        else if (!strcmp(command, "cos"))
-        {
-            WRITE_SINGLE_COMMAND(9);
-        }
-        
-        else if (!strcmp(command, "tan"))
-        {
-            WRITE_SINGLE_COMMAND(10);
-        }
-
-        else if (!strcmp(command, "in"))
-        {
-            WRITE_SINGLE_COMMAND(11);
-        }
-        
-        else if (!strcmp(command, "hlt"))
-        {
-            WRITE_SINGLE_COMMAND(12);
-        }
         else
         {
             error = INVALID_SYNTAX;
             print_assembler_error(error, text->linesPtr[i], i+1);
             break;
         }
-        
     }
 
     if (error)
